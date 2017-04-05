6e114a2909638418df5578389ec95e737a152e6e
juliuswang0728 <juliuswang0728@gmail.com>
2017-04-05 09:33:28 +0300

    revise training script

--- run_triton.sh
+++ run_triton.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Request 1 GPU
-#SBATCH --gres=gpu:m2090:1
+#SBATCH --gres=gpu:teslak80:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH -p gpushort
-#SBATCH --mem-per-cpu 4G
+#SBATCH --mem-per-cpu 8G
 #SBATCH -t 00-04
 #SBATCH --mail-user=juliuswang0728@gmail.com
 #SBATCH --mail-type=ALL
